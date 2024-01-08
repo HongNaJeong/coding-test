50561388f9f9b573461e39cbf5aab9d13337d24f
Na_Co2 <106801564+HongNaJeong@users.noreply.github.com>
2024-01-08 15:49:42 +0900

    Solution2 청소
    문제는 pdf파일에 있습니다

---
+++ Solution2/Soloution2.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class Soloution2 {
+    public int[] solution(int[][] board, int k) {
+        int[] answer = new int[2];
+        int n = board.length;
+        // 방향 설정
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int x = 0, y = 0, d = 1, count = 0;
+
+        while(count < k) {
+            count++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            // 범위 값을 벗어나거나 장애물을 만날 경우
+            if(nx < 0 || nx >= n || ny < 0 || ny >=n || board[nx][ny] == 1) {
+                d = (d + 1) % 4;
+                continue;
+            }
+            x = nx;
+            y = ny;
+        }
+        answer[0] = x;
+        answer[1] = y;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Soloution2 T = new Soloution2();
+        int[][] arr1 = {{0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr1, 10)));
+        int[][] arr2 = {{0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr2, 20)));
+        int[][] arr3 = {{0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0}};
+        System.out.println(Arrays.toString(T.solution(arr3, 25)));
+    }
+}
